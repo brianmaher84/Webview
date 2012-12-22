abd3e1e2245c43788c26861017bff01651a51531
Brian Maher <maherb@gmail.com>
2012-12-22 18:07:28 +0000

    Initial Commit
    After project create

---
+++ src/com/brim4brim/ptsb/PTSBActivity.java
@@ -0,0 +1,41 @@
+package com.brim4brim.ptsb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class PTSBActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webview = new WebView(this);
+        
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+          public void onProgressChanged(WebView view, int progress) {
+            // Activities and WebViews measure progress with different scales.
+            // The progress meter will automatically disappear when we reach 100%
+            activity.setProgress(progress * 1000);
+          }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+          public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+          }
+        });
+
+        webview.loadUrl("https://m.open24.ie/LogOn/Step1.aspx");
+    }
+}
