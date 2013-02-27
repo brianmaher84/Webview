5ba0657d1f593ffc5690fca9ae7638c2e561dfb4
Brian Maher <maherb@gmail.com>
2013-02-27 01:03:42 +0000

    back like browser

--- src/com/brim4brim/webview/WebviewActivity.java
+++ src/com/brim4brim/webview/WebviewActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -75,5 +76,17 @@ public class WebviewActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		return new MainMenuSelectedHandler(this)
 				.handleMenuSelect(item, webview);
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    // Check if the key event was the Back button and if there's history
+	    if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+	        webview.goBack();
+	        return true;
+	    }
+	    // If it wasn't the Back key or there's no web page history, bubble up to the default
+	    // system behavior (probably exit the activity)
+	    return super.onKeyDown(keyCode, event);
 	}
 }
